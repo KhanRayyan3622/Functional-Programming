4301babffea3b4a36befac30410d5d4c768ff224
Rayyan Khan <khan_rayyan22sept@outlook.com>
2023-11-07 02:31:36 +0500

    Update task3.scala

--- Assignment1/task3.scala
+++ Assignment1/task3.scala
@@ -1,1 +1,15 @@
-import scala.collection.mutable
+object Rayyan {
+    def main(args: Array[String]): Unit = {
+      def sum(x: Array[Double], y: Array[Double]): Array[Double] = {
+        x.zipWithIndex.map { case (num, index) => num + y(index) }
+      }
+
+      val a = Array[Double](1, 2, 3)
+      val b = Array[Double](4, 5, 6)
+
+      val sumResult = sum(a, b)
+      sumResult.foreach(println)
+    }
+  }
+
+
