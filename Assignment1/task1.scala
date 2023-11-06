53946c01746be6f6e9be51514d4c986e1da84c1a
Rayyan Khan <khan_rayyan22sept@outlook.com>
2023-11-07 02:05:58 +0500

    Update task1.scala

--- Assignment1/task1.scala
+++ Assignment1/task1.scala
@@ -1,1 +1,33 @@
+import scala.util.Random
+import scala.collection.mutable
 
+object Rayyan {
+  def main(args: Array[String]): Unit = {
+    val ran_num = new Random()
+    val n = ran_num.nextInt(500)
+    println(n)
+
+    val mylist = scala.collection.mutable.ListBuffer[Int]()
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+    mylist.append(ran_num.nextInt(500))
+
+    println(mylist)
+    val sortedList = mylist.sorted
+    println(sortedList)
+
+    val mapName = sortedList.zip(mylist).toMap
+    println(mapName)
+  }
+}
