5a185fadba23b797d2c0fe26771d45ba581092fa
Rayyan Khan <khan_rayyan22sept@outlook.com>
2023-11-07 02:20:03 +0500

    Update task2.scala

--- Assignment1/task2.scala
+++ Assignment1/task2.scala
@@ -1,1 +1,14 @@
+object Rayyan {
+  def main(args: Array[String]): Unit = {
+    val startAscii = 97 // ASCII value for 'a'
+    val endAscii = 122 // ASCII value for 'z'
 
+    def charArray(startNum: Int): List[Char] = {
+      (startNum to endAscii).map(_.toChar).toList
+    }
+
+    val charList = charArray(startAscii)
+
+    println("Character List: " + charList.mkString(", "))
+  }
+}
