d8654bc7cc397849c44b1a6726d56c5adedf4103
Rayyan Khan <khan_rayyan22sept@outlook.com>
2023-11-07 02:39:31 +0500

    Update task4.scala

--- Assignment1/task4.scala
+++ Assignment1/task4.scala
@@ -1,1 +1,16 @@
+object Rayyan {
+  def main(args: Array[String]): Unit = {
+    def even1(n: Array[Int]): Int = {
+      n.count(_ % 2 == 0)
+    }
 
+    def even2(n: Array[Int]): Int = {
+      n.filter(_ % 2 == 0).length
+    }
+
+    val array: Array[Int] = Array.fill(100)(util.Random.nextInt(100))
+
+    println(even1(array))
+    println(even2(array))
+  }
+}
